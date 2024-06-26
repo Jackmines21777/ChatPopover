ff2adb3d8985ece6cc66044743011a8861303ac2
Jackmines21777 <jack21777@outlook.com>
2024-06-26 16:07:02 -0500

    260624

---
+++ worker.js
@@ -0,0 +1,16 @@
+// onmessage = (e) => {
+//     console.log(e)
+//     console.log("Worker message received from main thread")
+// }
+
+
+import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
+
+const handler = new WebWorkerMLCEngineHandler();
+
+onmessage = (msg) => {
+    handler.onmessage(msg);
+    console.log(msg)
+};
+
+
